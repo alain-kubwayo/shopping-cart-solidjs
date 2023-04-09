e8d006cc632eca35210e42d8801b4fedead64809
alain-kubwayo <ahkubwayo@gmail.com>
2023-04-09 18:37:19 +0200

    feat: add and style card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 const Card = () => {
     return ( 
-        <div>
+        <div class="bg-white p-4 text-center rounded-md shadow-sm">
             <h2>Card Component</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam saepe odio iure, tempora molestiae modi?</p>
             <button class="btn">Click Me</button>
