3a0672f6c2c04344f3f8718d5d300987d9595ae3
alain-kubwayo <ahkubwayo@gmail.com>
2023-04-09 19:19:57 +0200

    feat: add signals with createSignal

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,18 @@ function App() {
       </header>
       <img class="rounded-md" src={banner} alt="site banner" />
       <div class="grid grid-cols-4 gap-10 my-4">
-        <Card />
-        <Card />
-        <Card />
+        {/* <Card /> */}
+        <Card rounded={true} flat={false}>
+          <h2>T-shirt - Black</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam saepe odio iure, tempora molestiae modi?</p>
+          <button class="btn">View</button>
+        </Card>
+        <Card rounded={false} flat={true}>
+          <h2>Hoodie - White</h2>
+          <button class="btn">View</button>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam saepe odio iure, tempora molestiae modi?</p>
+          <p>Only $10</p>
+        </Card>
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,65 @@
-const Card = () => {
+import { createSignal } from "solid-js";
+
+const Card = (props) => {
+    const [name, setName] = createSignal('alain');
+    const [bool, setBool] = createSignal(true);
+    const [age, setAge] = createSignal(25);
+    const [person, setPerson] = createSignal({
+        name: {
+            first: "Alain",
+            last: "Kubwayo"
+        },
+        age: 27
+    })
+
+    setTimeout(() => {
+        setName('James');
+    }, 1000)
+
+    setTimeout(() => {
+        // setBool(false);
+        // setBool(!bool()); // prevValue
+        // setBool(() => {
+        //     return false;
+        // })
+        setBool(prev => !prev);
+
+    }, 2000)
+
+    const ageInterval = setInterval(() => {
+        // setAge(prevAge => prevAge + 1);
+        setAge(age() + 1);
+    }, 1000);
+
+    clearInterval(ageInterval);
+
+    // createSignal is not really optimized for deeply nested objects or arrays.
+    // we couldn't pass in the last name directly, instead, we would have to re-write the whole object. It's a drawback. Instead, we use another function called createStore.
+    setTimeout(() => {
+        setPerson({
+            name: {
+                first: "Alain",
+                last: "Imanishimwe"
+            },
+            age: 27
+        })
+    }, 3000)
+
     return ( 
-        <div class="bg-white p-4 text-center rounded-md shadow-sm">
-            <h2>Card Component</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam saepe odio iure, tempora molestiae modi?</p>
-            <button class="btn">Click Me</button>
+        <div 
+            class="bg-white p-4 text-center"
+            classList={
+                { 
+                    "rounded-md": props.rounded, 
+                    "shadow-md": !props.flat 
+                }
+            }
+        >
+            <p>My name is {name()}</p>
+            <p>The boolean is {bool().toString()}</p>
+            <p>The age is {age()}</p>
+            <p>My name is {person().name.first} {person().name.last} and age is {person().age}</p>
+            { props.children }
         </div>
     );
 }
