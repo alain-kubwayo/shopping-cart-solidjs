6c5ae09e704b2feeb97ebd4ca8b5e2ccb8729b4b
alain-kubwayo <ahkubwayo@gmail.com>
2023-04-10 22:15:23 +0200

    feat: style the brand name

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div class="container m-auto">
       <header>
-        <h1>Alain's Shop</h1>
+        <h1 class="uppercase text-xl text-gray-700 font-semibold my-4">Alain's Shop</h1>
       </header>
       <div class="h-32 bg-[url('./assets/banner.webp')] bg-cover bg-center">
       </div>
