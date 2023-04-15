04bcabec43c8035c226389e64551cdba1ba36c56
alain-kubwayo <ahkubwayo@gmail.com>
2023-04-15 19:56:12 +0200

    feat: add theme to header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { createSignal } from 'solid-js';
 import banner from './assets/banner.webp';
 import Card from './components/Card';
 
 function App() {
+  const [darkTheme, setDarkTheme] = createSignal(false);
+
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme());
+  }
+
   return (
     <div class="container m-auto">
       <header>
-        <h1 class="uppercase text-xl text-gray-700 font-semibold my-4">Alain's Shop</h1>
+        <span
+          onClick={toggleTheme}
+        >Dark Mode</span>
+        <h1 
+          class="uppercase text-xl text-gray-700 font-semibold my-4"
+          classList={{ 
+            "bg-neutral-900": darkTheme(),
+            "text-white": darkTheme()
+          }}
+        >Alain's Shop</h1>
       </header>
       <div class="h-32 bg-[url('./assets/banner.webp')] bg-cover bg-center">
       </div>
