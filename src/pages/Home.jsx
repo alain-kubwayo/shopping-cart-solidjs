6997c9aba23cf57c2d94078601e6e540ecfdd561
alain-kubwayo <ahkubwayo@gmail.com>
2023-04-16 11:02:17 +0200

    feat: fetch products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,14 @@
+import { createResource } from "solid-js";
 import Card from "../components/Card";
 
+const fetchProducts = async () => {
+    const response = await fetch('http://localhost:4000/products');
+    return response.json();
+}
+
 const Home = () => {
+    const [products] = createResource(fetchProducts);
+
     return (
         <div class="grid grid-cols-4 gap-10 my-4">
         <Card rounded={true} flat={false}>
@@ -14,6 +22,8 @@ const Home = () => {
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam saepe odio iure, tempora molestiae modi?</p>
           <p>Only $10</p>
         </Card>
+
+        <p>{console.log(products(), products.loading)}</p>
       </div> 
 
     );
